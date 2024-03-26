89ddbb92de7a0f104a145e5c6211102ffd41798b
samuelaraag <samoelgaaradossantos@gmail.com>
2024-03-26 19:37:16 -0300

    deploy metodo incremento

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-A2KEKT26.mjs";import"./chunk-FH757QAU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-TH2YFPBO.mjs";import"./chunk-LV3X7J6J.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
