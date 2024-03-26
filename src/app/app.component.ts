b052e5fb79ab6a60cd383479a2e37cad73a8d101
samuelaraag <samoelgaaradossantos@gmail.com>
2024-03-26 19:33:11 -0300

    adicionado metodo de incremento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'biblioteca';
+  contador = 0;
+
+  adicionarContador(){
+    this.contador = ++this.contador;
+  }
 }
