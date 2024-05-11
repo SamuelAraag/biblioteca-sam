0845b5c33d315b71b5f39c92fb5eaf83883338b0
samuelaraag <samoelgaaradossantos@gmail.com>
2024-05-10 23:06:14 -0300

    comecando a tela principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Livro } from './modelos/livro.model';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,11 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'biblioteca';
   contador = 0;
-
+  livro: Livro = {
+    titulo: "COMECE PELO PORQUE",
+    linkImagem: "https://m.media-amazon.com/images/I/71M6czhxmTL._SL1500_.jpg"
+  }
+  
   aoAdicionarContador(){
     this.contador = ++this.contador;
   }
